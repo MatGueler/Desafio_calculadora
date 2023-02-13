06d824bce781f93bf89873f1f50e06c373fe453a
Mateus Gueler <mateus.gueler@gmail.com>
2023-02-13 19:49:11 -0300

    :dart: chore:
    Create functions to use calculator with all operations and finish layout

---
+++ src/Components/Calculator/Calculator_component.jsx
@@ -0,0 +1,9 @@
+import { Calculator } from './Calculator_style';
+
+class CalculatorComponent {
+	render = () => {
+		return <Calculator>soma</Calculator>;
+	};
+}
+
+export default CalculatorComponent;
